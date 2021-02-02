a735a913c3b4bf21595adc17af2674eeb0c7526b
danghualong <dhl_345@qq.com>
2021-02-02 18:37:59 +0800

    初始化state

--- src/todos/reducers/index.ts
+++ src/todos/reducers/index.ts
@@ -2,10 +2,10 @@ import { Todo } from "../../entity"
 import { AboutTodoAction } from "../actions"
 import {Constants} from "../../constants"
 
-export default (state: Todo[]=[{id:1,content:"打开",completed:true},{id:2,content:"关闭",completed:false}], action: AboutTodoAction)=> {
+export default (state: Todo[]=[], action: AboutTodoAction)=> {
     switch (action.type) {
         case Constants.ADD_TODO_ACTION:
-            let maxId = Math.max.apply(Math, state.map(p => p.id))
+            let maxId = state.length==0?0: Math.max.apply(Math, state.map(p => p.id))
             const item = { id: maxId + 1, content: action.content, completed: false };
             return [
                 item,
